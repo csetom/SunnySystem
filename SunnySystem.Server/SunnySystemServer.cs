66c95c559258f2a3c4447c1cba60f085f2d6beba
Tamas Csesznak <tcses@cae.com>
2023-02-25 20:11:06 +0100

    Server Beggining

---
+++ SunnySystem.Server/SunnySystemServer.cs
@@ -0,0 +1,13 @@
+﻿namespace SunnySystem.Server;
+using SunnySystem.Repository;
+using SunnySystem.Data.Models;
+public class SunnySystemServer
+{
+  private readonly IMyRepository<Componentsmain> componentRepository;
+  public SunnySystemServer(IMyRepository<Componentsmain> IcomponentsMainRepository) => componentRepository = IcomponentsMainRepository;
+  public void WriteAllComponents() {
+      Console.WriteLine(componentRepository.GetAll().First().ToString());
+  }
+
+
+}
--- SunnySystem/Program.cs
+++ SunnySystem/Program.cs
@@ -1,15 +1,17 @@
 ﻿using SunnySystem.Data;
 using SunnySystem.Repository;
+using SunnySystem.Server;
 namespace SunnySystem;
 internal class Program
 {
   private static void Main(string[] args)
   {
+
     using (var ctx = new SunnySystem.Data.LnghydvrContext())
     {
-      ComponentRepository C=new ComponentRepository(ctx); 
-      Console.WriteLine(C.GetAll().First().ToString());
-        }
+      SunnySystemServer server  = new SunnySystemServer( new ComponentRepository(ctx));
+      server.WriteAllComponents();
+    }
   }
 
 }
