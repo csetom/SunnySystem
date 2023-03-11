5cb51e42db8196a47ee57d6bcc8ef16c5d14d91d
Tamas Csesznak <tcses@cae.com>
2023-03-11 12:31:57 +0100

    feat: project and customer begining

---
+++ Server/SunnySystem.Data/Models/Project.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SunnySystem.Data.Models
+{
+
+
+public class Project
+{
+    [Key]
+    [Column("project_id")]
+    public int ProjectID { get; set; }
+    [Column("project_name")]
+    public string? ProjectName { get; set; }
+    [Column("start_date")]
+    public DateTime StartDate { get; set; }
+    [Column("end_date")]
+    public DateTime EndDate { get; set; }
+    [Column("customer_id")]
+    public int CustomerId {get;set;}
+}
+
+}
